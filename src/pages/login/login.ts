166a3655f2820d2f9eaa3002d60e150ab5aabbf6
Augusto Romero <geragusto@hotmail.com>
2018-01-11 02:10:14 -0300

    errrors Form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, ToastController } from 'ionic-angular';
 import {AuthProvider} from '../../providers/auth/auth';
+import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 import {Credencial} from '../../app/interfaces/credencial.interface';
-import {NgForm} from '@angular/forms';
 /**
  * Generated class for the LoginPage page.
  *
@@ -16,17 +16,43 @@ import {NgForm} from '@angular/forms';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  credenciales:Credencial={
-    email:"",
-    password:""
-  }
-  constructor(private authProvider:AuthProvider) {
+  formLogin:FormGroup;
+  constructor(private authProvider:AuthProvider,
+              private toastCtrl:ToastController) {
+    this.formLogin=new FormGroup({
+      'email':new FormControl('',[Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'password':new FormControl('',Validators.required),
+    });
   }
 
   // ionViewDidLoad() {
   //   console.log('ionViewDidLoad LoginPage');
   // }
-  login(loginForm:NgForm){
-    console.log(loginForm);
+  login(){
+    console.log(this.formLogin);
+    if (this.formLogin.valid){
+
+    }
+    else{
+      if(this.formLogin.controls['email'].errors && this.formLogin.controls['password'].errors && this.formLogin.controls['email'].errors.required && this.formLogin.controls['password'].errors.required){
+        this.presentToast("Debe ingresar sus datos");
+      }else if(this.formLogin.controls['email'].errors && this.formLogin.controls['email'].errors.pattern){
+        this.presentToast("Formato de correo invalido");
+      }else if (this.formLogin.controls['email'].errors && this.formLogin.controls['email'].errors.required){
+        this.presentToast("Debe ingresar su email");
+      }else if (this.formLogin.controls['password'].errors && this.formLogin.controls['password'].errors.required){
+        this.presentToast("Debe ingresar su contraseña");
+      }
+    }
   }
+  presentToast(mensaje:string) {
+   let toast = this.toastCtrl.create({
+       message: mensaje,
+       position: 'bottom',
+       showCloseButton:true,
+       closeButtonText:"OK"
+     });
+
+     toast.present();
+   }
 }
