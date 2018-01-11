ea5d968df41ae78d907a4fd52b3820374f1f70c1
Augusto Romero <geragusto@hotmail.com>
2018-01-11 11:20:23 -0300

    Clase Hermano

---
+++ src/app/clases/Hermano.class.ts
@@ -0,0 +1,14 @@
+export class Hermano{
+  nombre:string;
+  userId:string;
+  fechaNacimiento:string;
+  domicilio:string;
+  telefono:number;
+  familia:string;
+  publicador:boolean;
+  bautizado?:boolean;
+  fechaBautismo?:string;
+  precursorRegular?:boolean;
+  siervoMinisterial?:boolean;
+  anciano?:boolean;
+}
